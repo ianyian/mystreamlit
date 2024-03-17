b3573c0f712b4bc077aa390ec3c5dc8f6a3f658e
XJ <ianyian@gmail.com>
2024-03-17 21:51:41 +0800

    update

--- myapp.py
+++ myapp.py
@@ -18,3 +18,9 @@ tickerDf = tickerData.history(period="1mo", start="2010-5-31", end="2020-5-31")
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
+
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+html = pd.read_html(url, header=0)
+df = html[0]
+
+df
